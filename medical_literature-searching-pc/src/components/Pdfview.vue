22e6e5e58851f48e667203989d81b9b1ab6f5a23
qingrujiansu <2789930689@qq.com>
2023-01-28 14:36:06 +0800

    使用pdf-embed

--- medical_literature-searching-pc/src/components/Pdfview.vue
+++ medical_literature-searching-pc/src/components/Pdfview.vue
@@ -1,18 +1,17 @@
 <template>
     <div class="pdf-preview">
         <div class="pdf-wrap">
-            <vue-pdf-embed :source="state.source" :style="scaleFun" class="vue-pdf-embed" :page="state.pageNum" />
-        </div>
-        <div class="page-tool">
-            <div class="page-tool-item" @click="lastPage">上一页</div>
-            <div class="page-tool-item" @click="nextPage">下一页</div>
-            <div class="page-tool-item">{{ state.pageNum }}/{{ state.numPages }}</div>
-            <div class="page-tool-item" @click="pageZoomOut">放大</div>
-            <div class="page-tool-item" @click="pageZoomIn">缩小</div>
-            <el-button type="success" icon="Document" circle class="downLoad" size="small" />
+            <vue-pdf-embed  :source="state.source" :style="scaleFun" :page="state.pageNum" />
         </div>
     </div>
-
+    <div class="page-tool">
+        <div class="page-tool-item" @click="lastPage">上一页</div>
+        <div class="page-tool-item" @click="nextPage">下一页</div>
+        <div class="page-tool-item">{{ state.pageNum }}/{{ state.numPages }}</div>
+        <div class="page-tool-item" @click="pageZoomOut">放大</div>
+        <div class="page-tool-item" @click="pageZoomIn">缩小</div>
+        <el-button type="success" icon="Document" circle class="downLoad" size="small" />
+    </div>
 </template>
 
 <script setup lang='ts'>
@@ -66,8 +65,30 @@ onMounted(() => {
 </script>
 
 <style lang='less' scoped>
+.pdf-preview {
+    position: relative;
+    height: 100vh;
+    box-sizing: border-box;
+    background-color: e9e9e9;
+}
+
+.pdf-wrap {
+    overflow-y: scroll;
+    // height: 900px;
+    height: 100%;
+}
+
+vue-pdf-embed {
+    text-align: center;
+    width: 75%;
+    border: 1px solid #e5e5e5;
+    margin: 0 auto;
+    box-sizing: border-box;
+}
+
+
 .page-tool {
-    position: fixed;
+    position: absolute;
     bottom: 10px;
     padding-left: 15px;
     padding-right: 15px;
@@ -86,11 +107,5 @@ onMounted(() => {
     padding: 8px 15px;
     padding-left: 10px;
     cursor: pointer;
-}
-
-.downLoad {
-    position: flex;
-    top: 10px;
-    left: 10px;
 }
 </style>
